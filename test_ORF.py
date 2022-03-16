570800f30838ed0bcd833f315e77c868899ad440
nienkegertsen <71077892+nienkegertsen@users.noreply.github.com>
2022-03-16 10:36:37 +0100

    ORF_finder af

--- test_ORF.py
+++ test_ORF.py
@@ -1,54 +1,101 @@
-header = ">sdjlkfdsj33"
-sequentie = "ATGATAGATAATGGATAACAGATACAAGTATAGACAATcccTTTAACCAGGA"
+def read_Scaffold(file_scaffold):
+    """
+    Reads in a FASTA-file containing a scaffold.
+    :param file_scaffold: String - file name
+    :return: scaf_header - String - header of scaffold
+             scaf_seq - String - sequence of scaffold
+    """
+    scaf_header = "" # instantiates String scaf_header
+    scaf_seq = ""      # instantiates String scaf_seq
+    with open(file_scaffold, "r") as scaf_file:
+        for line in scaf_file:
+            if line.startswith(">"):
+                scaf_header = line.strip()
+            else:
+                scaf_seq = scaf_seq.upper() # Sets sequence in uppercase
+                scaf_seq += line.strip()
 
-listOfOrf = list()
-frames = []  # storing the six frame translation that it zould be extacted from the fragments
-dna = sequentie  # extract the fragment
-description = header  # extact the desciption even were not use it, just for learning purpose
-reverseCdna = []  # storing the reverse compliments
-listOfFrame = []  # list of frames
-# create the positive frames
-# split the frames into codons for better performance
-frames.append([dna[i:i + 3] for i in range(0, len(dna), 3)])
-frames.append([dna[i:i + 3] for i in range(1, len(dna), 3)])
-frames.append([dna[i:i + 3] for i in range(2, len(dna), 3)])
-# reverse compliment of the fragment
-reverse = {"A": "T", "C": "G", "T": "A", "G": "C"}
-for i in range(len(dna)):
-    reverseCdna.append(reverse[dna[-i - 1]]) if dna[-i - 1] in reverse.keys() else reverseCdna.append(
-        dna[-i - 1])  # if any contamination found we keep it for further more check
-reverseCdna = ''.join(reverseCdna)  # joining
-# create the negative frames
-frames.append([reverseCdna[i:i + 3] for i in range(0, len(reverseCdna), 3)])
-frames.append([reverseCdna[i:i + 3] for i in range(1, len(reverseCdna), 3)])
-frames.append([reverseCdna[i:i + 3] for i in range(2, len(reverseCdna), 3)])
+    return scaf_header, scaf_seq
 
-for i in range(0,len(frames),1): #looping all the frames
-    start=0
-    while start <len(frames[i]): #looping each frame for stop and stop codons
-        if frames[i][start]=="TAA" or frames[i][start]=="TAG" or frames[i][start]=="TGA":
-            for stop in range(start+1,len(frames[i]),1):
-                         if frames[i][stop]=="TAA" or  frames[i][stop]=="TAG" or  frames[i][stop]=="TGA" :
-                                listOfOrf.append(frames[i][start:stop]) # retrieve the orf
-                                listOfFrame.append(i)
-                                start=stop+1 # avoiding multiple start codons
-                                break
-        start+=1
+def Frame_finder(scaf_header, scaf_seq):
+    """
+    Storing the six frame translation that it should be extacted from the fragments.
+    :param scaf_header: String - header of scaffold
+    :param scaf_seq:  String - sequence of scaffold
+    :return: frames: list - storing the six frame translation
+             description -  String header of scaffold
+    """
+    frames = []  # storing the six frame translation that it should be extacted from the fragments
+    dna = scaf_seq  # extract the fragment
+    description = scaf_header # extact the desciption
+    reverseCdna = []  # storing the reverse compliments
+    # create the positive frames
+    # split the frames into codons for better performance
+    frames.append([dna[i:i + 3] for i in range(0, len(dna), 3)])
+    frames.append([dna[i:i + 3] for i in range(1, len(dna), 3)])
+    frames.append([dna[i:i + 3] for i in range(2, len(dna), 3)])
+    # reverse compliment of the fragment
+    reverse = {"A": "T", "C": "G", "T": "A", "G": "C"}
+    for i in range(len(dna)):
+        reverseCdna.append(reverse[dna[-i - 1]]) if dna[-i - 1] in reverse.keys() else reverseCdna.append(
+            dna[-i - 1])  # if any contamination found we keep it for further more check
+    reverseCdna = ''.join(reverseCdna)  # joining
+    # create the negative frames
+    frames.append([reverseCdna[i:i + 3] for i in range(0, len(reverseCdna), 3)])
+    frames.append([reverseCdna[i:i + 3] for i in range(1, len(reverseCdna), 3)])
+    frames.append([reverseCdna[i:i + 3] for i in range(2, len(reverseCdna), 3)])
 
-print(listOfOrf)
+    return frames, description
 
-with open("ORF.fa", "w") as file:
-    for orf, frame in zip(listOfOrf, listOfFrame):
-        seq = ""
-        for i in orf:
-            seq += i
-        file.write(description + " / " + str(frame))
-        file.write("\n")
-        file.write(seq)
-        file.write("\n")
-file.close()
+def ORF_finder(frames):
+    """
+    Looping over all frames and looking for ORFs from stop to stop codons. The ORFs found were
+    stored into the list listOfOrf.
+    :param frames: list - storing the six frame translation.
+    :return: listOfOrf - list - storing the ORFs found.
+             listOfFrame - list - storing the used reading frames for each ORF.
+    """
+    listOfOrf = list()
+    listOfFrame = []  # list of frames
+    for i in range(0,len(frames),1): #looping all the frames
+        start=0
+        while start <len(frames[i]): #looping each frame for stop and stop codons
+            if frames[i][start]=="TAA" or frames[i][start]=="TAG" or frames[i][start]=="TGA":
+                for stop in range(start+1,len(frames[i]),1):
+                             if frames[i][stop]=="TAA" or  frames[i][stop]=="TAG" or  frames[i][stop]=="TGA" :
+                                    listOfOrf.append(frames[i][start:stop]) # retrieve the orf
+                                    listOfFrame.append(i)
+                                    start=stop+1 # avoiding multiple start codons
+                                    break
+            start+=1
+    return listOfOrf, listOfFrame
 
 
+def Write_Orf_Fasta(listOfOrf, listOfFrame, description):
+    """
+    Writes all found ORF to a file.
+    :param listOfOrf: list - storing the ORFs found.
+    :param listOfFrame: list - storing the used reading frames for each ORF.
+    :param description: String header of scaffold
+    :return:
+    """
+    counter = 0
+    with open("ORF.fa", "w") as file:
+        for orf, frame in zip(listOfOrf, listOfFrame):
+            counter += 1
+            seq = ""
+            for i in orf:
+                seq += i
 
+            file.write(description + "+" + str(counter) + " / " + str(frame))
+            file.write("\n")
+            file.write(seq)
+            file.write("\n")
+            seq = ""
+    file.close() # closed the file.
 
-
+if __name__ == '__main__':
+    scaf_header, scaf_seq = read_Scaffold(file_scaffold= "SD (1).fa")
+    frames, description = Frame_finder(scaf_header, scaf_seq)
+    listOfOrf, listOfFrame = ORF_finder(frames)
+    Write_Orf_Fasta(listOfOrf, listOfFrame, description)
