918d8c8c2bfd96bee4e9f0776533aa344a863c97
DilonAndriesse <70940653+DilonAndriesse@users.noreply.github.com>
2022-03-25 10:14:10 +0100

    Update BLAST.py
    vernieuwde versie van de blast

--- BLAST.py
+++ BLAST.py
@@ -1,10 +1,73 @@
 from Bio.Blast import NCBIWWW
+from time import sleep
 
 
-def blastN():
-    fasta_string = open("ons_bestand.fasta").read()
-    result_handle = NCBIWWW.qblast("blastn", "nt", fasta_string)
+# def read_orf(file):
+#     """
+#     Reads and places the ORFs from the ORF-finder generated file in a list.
+#
+#             Parameters:
+#                     file (file): File containing ORFs generated with the ORF-finder class
+#
+#             Returns:
+#                     orf_list (list): A list containing all the ORFs from the ORF-file
+#     """
+#     seq = ""
+#     orf_headers, orf_seqs = [], []
+#
+#     with open(file) as file:
+#         for line in file:
+#             if line.startswith(">"):
+#                 if seq == "":
+#                     header = line.strip()
+#                 else:
+#                     headers.append(header)
+#                     seqs.append(seq)
+#                     header = line.strip()
+#                     seq = ""
+#             else:
+#                 seq += line.strip()
+#         seqs.append(seq)
+#
+#     return orf_headers, orf_seqs
+#
+#
+# def parameters_check_blastn(parameters):
+#     """
+#     Checks the user given parameters for invalid values.
+#
+#             Parameters:
+#                     parameters (list): A list of all adjustable parameters,
+#
+#             Returns:
+#     """
+#     # afhankelijk van hoe parameters doorgegeven worden.
 
 
-def wegSchrijven():
-    print("hoi")
+def blastn():
+    """
+    BLASTs the ORFs
+
+            Parameters:
+                    seqs (list): List of ORF sequences
+
+            Returns:
+
+    """
+    sequence_data = open("orf__all.fa").read()
+    result_handle = NCBIWWW.qblast("blastp", "refseq_protein", sequence_data, entrez_query="txid4892[ORGN]", hitlist_size=10)
+
+    #with open("ORF_XML/ORF%s.xml" % i, "w") as out_handle:
+    with open("ORF_XML/ORF.xml", "w") as out_handle:
+        out_handle.write(result_handle.read())
+    result_handle.close()
+
+    print("klaar")
+
+
+if __name__ == "__main__":
+    #orf_file = "ORF__all.fa"
+
+    #headers, seqs = read_orf(orf_file)
+    #parameters_check_blastn(parameters)
+    blastn()
