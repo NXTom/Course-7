51bb1a669a5d958026e99152ce2c8e9edf290edd
DilonAndriesse <70940653+DilonAndriesse@users.noreply.github.com>
2022-03-25 12:30:38 +0100

    Update BLAST.py
    parameter nummering aanpassing en error afvangen

--- BLAST.py
+++ BLAST.py
@@ -13,20 +13,22 @@ def parameters_check_blastn(selection_BLAST):
     """
     evalue = True
     # expect: afhankelijk van hoe e-value wordt meegegeven vanuit GUI
-    if 0 >= selection_BLAST[1] >= 1:
+    if selection_BLAST[1] != int:
+        evalue = False
+    elif 0 >= float(selection_BLAST[1]) >= 1:
         evalue = False
 
     # organism adjustments
-    if selection_BLAST[4] == "Eukaryote":
-        selection_BLAST[4] = "txid2759[ORGN]"
-    elif selection_BLAST[4] == "Prokaryote":
-        selection_BLAST[4] = "txid2[ORGN]"
-    elif selection_BLAST[4] == "Archaea":
-        selection_BLAST[4] = "txid2157[ORGN]"
-    elif selection_BLAST[4] == "Yeast":
-        selection_BLAST[4] = "txid4892[ORGN]"
-    elif selection_BLAST[4] == "Fungi":
-        selection_BLAST[4] = "txid4751[ORGN]"
+    if selection_BLAST[3] == "Eukaryote":
+        selection_BLAST[3] = "txid2759[ORGN]"
+    elif selection_BLAST[3] == "Prokaryote":
+        selection_BLAST[3] = "txid2[ORGN]"
+    elif selection_BLAST[3] == "Archaea":
+        selection_BLAST[3] = "txid2157[ORGN]"
+    elif selection_BLAST[3] == "Yeast":
+        selection_BLAST[3] = "txid4892[ORGN]"
+    elif selection_BLAST[3] == "Fungi":
+        selection_BLAST[3] = "txid4751[ORGN]"
 
     return evalue, selection_BLAST
 
@@ -45,9 +47,9 @@ def blastn(selection_BLAST):
                                    expect=selection_BLAST[1],
                                    word_size=selection_BLAST[2],
                                    # matrix_name=selection_BLAST[3],
-                                   entrez_query=selection_BLAST[4],
-                                   hitlist_size=selection_BLAST[5],
-                                   megablast=selection_BLAST[6])
+                                   entrez_query=selection_BLAST[3],
+                                   hitlist_size=selection_BLAST[4],
+                                   megablast=selection_BLAST[5])
 
     with open("ORF_XML/ORF.xml", "w") as out_handle:
         out_handle.write(result_handle.read())
@@ -61,5 +63,3 @@ def main_blast(selection_BLAST):
 
     if evalue:
         blastn(selection_BLAST)
-    else:
-        quit()
