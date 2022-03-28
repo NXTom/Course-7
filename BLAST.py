4a7a27aa1bd8808b3d511b090a7c322b5765981f
DilonAndriesse <70940653+DilonAndriesse@users.noreply.github.com>
2022-03-28 13:31:42 +0200

    Update BLAST.py

--- BLAST.py
+++ BLAST.py
@@ -1,15 +1,13 @@
 from Bio.Blast import NCBIWWW
+from Bio.Blast import NCBIXML
 
 
 def parameters_check_blastn(selection_BLAST):
     """
     Checks the user given parameters for invalid values.
-
-            Parameters:
-                    selection_BLAST (list): A list of all adjustable parameters
-
-            Returns:
-                    selection_BLAST (list): A list of BLAST parameters
+    :param selection_BLAST: list - list containing the BLAST parameters
+    :return evalue: boolean - if evalue is invalid returns false
+            selection_BLAST: list - returns parameters list with tax id
     """
     evalue = True
     # expect: afhankelijk van hoe e-value wordt meegegeven vanuit GUI
@@ -35,30 +33,63 @@ def parameters_check_blastn(selection_BLAST):
 
 def blastn(selection_BLAST):
     """
-    BLASTs the ORFs
-            Parameters:
-                    selection_BLAST (list): List of BLAST parameters
-            Returns:
-                    ORF.xml (file): one XML file with all BLAST results
+    Calling biopython's BLAST function to BLAST the ORFs
+    :param selection_BLAST: list - list containing the BLAST parameters
+    :return: ORF.xml: file - one XML file containing all BLAST results
     """
     sequence_data = open("ORF.fa").read()
     result_handle = NCBIWWW.qblast("blastn", selection_BLAST[0],
                                    sequence_data,
                                    expect=selection_BLAST[1],
                                    word_size=selection_BLAST[2],
-                                   # matrix_name=selection_BLAST[3],
                                    entrez_query=selection_BLAST[3],
                                    hitlist_size=selection_BLAST[4],
                                    megablast=selection_BLAST[5])
 
-    with open("ORF_XML/ORF.xml", "w") as out_handle:
-        out_handle.write(result_handle.read())
-    result_handle.close()
+    blast_records = NCBIXML.parse(result_handle)
+    xml_to_fasta(blast_records)
 
-    print("The BLAST has finished")
+    # with open("ORF.xml", "w") as out_handle:
+    #     out_handle.write(result_handle.read())
+    # result_handle.close()
+
+
+def xml_to_fasta(blast_records):
+    with open("ORF_func_anno.fa", "w") as file:
+        for blast_record in blast_records:
+            counter = 0
+            if blast_record.alignments:
+                counter = 0
+                file.write(blast_record.query + "\n")
+                file.write("-" * len(blast_record.query) + "\n")
+            for alignment in blast_record.alignments:
+                counter += 1
+                file.write("hit number " + str(counter) + "\n")
+                for hsp in alignment.hsps:
+                    file.write(
+                        "\thit description:" + alignment.title + "\n")
+                    file.write(
+                        "\tAccession:" + alignment.accession + "\n")
+                    file.write(
+                        "\tlength:" + str(alignment.length) + "\n")
+                    file.write("\te-value:" + str(hsp.expect) + "\n")
+                    file.write("\talignment length:" + str(
+                        hsp.align_length) + "\n")
+                    file.write(
+                        "\tpositives:" + str(hsp.positives) + "\n")
+                    file.write("\t" + hsp.query[0:] + "\n")
+                    file.write("\t" + hsp.match[0:] + "\n")
+                    file.write("\t" + hsp.sbjct[0:] + "\n")
+                    file.write("\n")
+
+
 
 
 def main_blast(selection_BLAST):
+    """
+    Calls the two functions from the BLAST script
+    :param selection_BLAST: list - list containing the BLAST parameters
+    """
     evalue, selection_BLAST = parameters_check_blastn(selection_BLAST)
 
     if evalue:
