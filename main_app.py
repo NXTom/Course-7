c9bedd5991f933e9f7812efc59fd8b68f9362b7f
NXTom <51476932+nxtom@users.noreply.github.com>
2022-03-31 13:04:42 +0200

    Documentatie

--- main_app.py
+++ main_app.py
@@ -3,12 +3,11 @@ import test_ORF
 
 
 def maintest(bestand, ORF_par, BLAST_par):
-   test_ORF.main_ORF(bestand, ORF_par)
-   print(ORF_par)
-   print(BLAST_par)
-   #BLAST.main_blast()
-   #print("doie")
-
-
-if __name__ == '__main__':
-    main()
+    """
+    Haalt alle benodigde parameters en variabelen op uit de GUI
+    Deze worden daarna verwerkt in ORF finder
+    De resultaten hieruit zijn voor de blast functie.
+    De blast functie slaat uiteindelijk alle resultaten op.
+    """
+    test_ORF.main_ORF(bestand, ORF_par)
+    BLAST.main_blast(BLAST_par)
