bb004d7a5cc81ddad77ea24ca53604012cb76b73
NXTom <51476932+NXTom@users.noreply.github.com>
2022-03-31 12:52:36 +0200

    MacOS+ Oude code verwijderen.

--- gui.py
+++ gui.py
@@ -9,8 +9,7 @@ def openFile():
     :return: filepath
     """
     global filepath
-    filepath = filedialog.askopenfilename(
-        initialdir="/Users/tomvandergreft/desktop")
+    filepath = filedialog.askopenfilename()
     file = open(filepath, "r")
    # print(file.read())
    # print(filepath)
